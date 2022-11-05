e10d43588f4f3dc96462d8ee7f503d858d7eeaa9
iXmalMonk <ixmal999@gmail.com>
2022-11-05 15:52:14 +0300

    import and packages

---
+++ Lab/src/main/java/com/human/Human.java
@@ -0,0 +1,21 @@
+package com.human;
+
+public class Human {
+    private int age = 0;
+
+    public Human(int _age)
+    {
+        if (_age >= 0){
+            age = _age;
+        }
+        else age = 0;
+    }
+
+    public void birthday() {
+        age++;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
