a7e4cb6bff6bfc8d2034faeac74bc6f70fbeebcc
iXmalMonk <ixmal999@gmail.com>
2022-11-05 14:10:18 +0300

    l

---
+++ Lab/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    // types
+    byte b = 0; // 1 byte // integer
+    short s = 1; // 2 bytes // integer
+    int i = 2; // 4 bytes // integer
+    long l = 3; // 8 bytes // integer
+    float f = 4.5f; // 4 bytes // real
+    double d = 6.7; // 8 bytes // real
+    boolean bool = true; // 1 bit // bool
+    char c = '9'; // 2 bytes // symbols
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
